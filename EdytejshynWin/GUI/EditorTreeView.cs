6fcc82d91774291a40b331939abe9533d8be7761
piomar123 <piomar123@gmail.com>
2015-03-26 00:23:27 +0100

    [Editor] Converted XNA in WinForms handling using official Microsoft code. Editor has its own Content.

---
+++ EdytejshynWin/GUI/EditorTreeView.cs
@@ -0,0 +1,10 @@
+﻿namespace Edytejshyn.GUI
+{
+    public class EditorTreeView : System.Windows.Forms.TreeView
+    {
+        public int Answer
+        {
+            get { return 42; }
+        }
+    }
+}
