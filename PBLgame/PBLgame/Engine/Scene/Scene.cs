73cbfc4e4bd80365c90d92d624ad8ae1ad89645d
Krzysztof Szczech <krzysztof.szczech@hotmail.com>
2015-03-29 09:14:41 +0200

    Started working with scene

---
+++ PBLgame/PBLgame/Engine/Scene/Scene.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using PBLgame.Engine.GameObjects;
+
+namespace PBLgame.Engine.Scene
+{
+    public class Scene
+    {
+        #region Variables
+        #region Private
+        //It's gonna be scene graph later
+        private List<GameObject> _sceneGameObjects;
+
+        #endregion
+        #region Protected
+        #endregion
+        #region Public
+        #endregion
+        #endregion
+
+        #region Properties
+
+
+
+        #endregion
+
+        #region Methods
+
+
+        #endregion
+    }
+}
