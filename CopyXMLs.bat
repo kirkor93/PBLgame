7842833c23f452a3d6b90e424f5f47964baf4d03
piomar123 <piomar123@gmail.com>
2015-04-08 21:23:27 +0200

    XMLs Copy script update

--- CopyXMLs.bat
+++ CopyXMLs.bat
@@ -1,2 +1,2 @@
-fsutil hardlink create "%1\content.xml" "%2\content.xml" || echo.
-fsutil hardlink create "%1\Scene 1.xml" "%2\Scene 1.xml" || echo.
+fsutil hardlink create "%1\content.xml" "%2\content.xml" > nul || echo x
+fsutil hardlink create "%1\Scene 1.xml" "%2\Scene 1.xml" > nul || echo x
