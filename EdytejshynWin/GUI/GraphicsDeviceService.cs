b473b74508e36286c11cdb91d1cf425364c707ec
piomar123 <piomar123@gmail.com>
2015-04-08 16:29:48 +0200

    [Editor] HiDef hotfix

--- EdytejshynWin/GUI/GraphicsDeviceService.cs
+++ EdytejshynWin/GUI/GraphicsDeviceService.cs
@@ -58,7 +58,7 @@ namespace Edytejshyn.GUI
             parameters.IsFullScreen = false;
 
             graphicsDevice = new GraphicsDevice(GraphicsAdapter.DefaultAdapter,
-                                                GraphicsProfile.Reach,
+                                                GraphicsProfile.HiDef,
                                                 parameters);
         }
 
